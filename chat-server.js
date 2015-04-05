7af3003666933ddd82b06afb992a75357e1e87d0
Hunter Wills <hwills@ip-172-31-19-165.us-west-2.compute.internal>
2015-04-04 19:08:28 -0500

    added joining rooms

--- chat-server.js
+++ chat-server.js
@@ -22,6 +22,10 @@ app.listen(3456);
 
 var loggedUsers = ["Guest"];
 var guestNumber = 0;
+var rooms = ["General"];
+var roomPasswords = [""];
+var roomSockets = [[]];
+var roomUsers = [[]];
 
 
  
@@ -36,12 +40,24 @@ io.sockets.on("connection", function(socket){
 		// This callback runs when the server receives a new message from the client.
  
 		console.log("message: "+data["message"]); // log it to the Node.JS output
-		io.sockets.emit("message_to_client",{message:data["message"] }) // broadcast the message to other users
+		console.log("to users: "+roomUsers); // log it to the Node.JS output
+		for(var i = 0; i < rooms.length; i++){
+			if(rooms[i] == data['toRoom']){
+				for(var j = 0; j < roomUsers[i].length; j++){
+					roomSockets[i][j].emit("message_to_client",{message:data["message"] }) // broadcast the message to other users
+				}
+			}
+			break;
+		}
+	});
+
+	socket.on('get_rooms_on_server', function(data) {
+		io.sockets.emit("return_rooms_to_client",{allRooms:rooms, index:rooms.length }) // broadcast the message to other users
 	});
 
 	socket.on('nickname_to_server', function(data) {
 		var isValid = true;
-		if(data["nickname"].length >= 5 && data["nickname"].substring(0, 6) == "Guest"){
+		if(data["nickname"].length >= 5 && data["nickname"].substring(0, 5) == "Guest"){
 			isValid = false;
 		}
 		else{
@@ -54,14 +70,33 @@ io.sockets.on("connection", function(socket){
 				}
 			}
 		}
+		var name = data["nickname"];
 		if(isValid){
 			loggedUsers.push(data["nickname"]);
-			socket.emit("validate_nickname_to_client", {nickname:data["nickname"]});
 		}
 		else{
 			guestNumber++;
-			socket.emit("validate_nickname_to_client", {nickname:"Guest" + guestNumber});
+			name = "Guest"+guestNumber;
 		}
+		var room = data['room'];
+		var otherUsers;
+		for(var i = 0; i < rooms.length; i++){
+			if(rooms[i]==data['room']){
+				if(roomPasswords[i] != "" && roomPasswords[i] != data['password']){
+					roomSockets[0].push(socket);
+					roomUsers[0].push(name);
+					otherUsers = roomUsers[0];
+					room = "General";
+				}
+				else{
+					roomSockets[i].push(socket);
+					roomUsers[i].push(name);
+					otherUsers = roomUsers[i];
+				}
+				break;
+			}
+		}
+		socket.emit("validate_nickname_to_client", {nickname:name, inRoom:room, users:otherUsers});
 	});
 });
 
