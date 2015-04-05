552e205bb041d6bbddcd348b0c608aa4afb8857c
Hunter Wills <hwills@ip-172-31-19-165.us-west-2.compute.internal>
2015-04-05 00:34:09 -0500

    finihsed all but creative

--- chat-server.js
+++ chat-server.js
@@ -27,7 +27,7 @@ var roomPasswords = [""];
 var roomSockets = [[]];
 var roomUsers = [[]];
 var bannedUsers = [[]];
-var roomCreators = ["devs"];
+var roomCreators = ["Guest"];
 
 function getId(name, the_array){
 	
@@ -84,11 +84,45 @@ io.sockets.on("connection", function(socket){
 
 
 	socket.on("add_room_on_server", function(data){
-		rooms.push(data["roomName"]);
-		roomPasswords.push(data["roomPass"]);
-		roomUsers.push([]);
-		roomCreators.push(data["creator"]);
-		roomSockets.push([]);
+		var exists = false;
+		for(var i = 0; i < rooms.length; i++){
+			if(rooms[i]==data['roomName']){
+				exists = true;
+			}
+		}
+		if(!exists){
+			rooms.push(data["roomName"]);
+			roomPasswords.push(data["roomPass"]);
+			roomUsers.push([]);
+			bannedUsers.push([]);
+			roomCreators.push(data["creator"]);
+			roomSockets.push([]);
+		}
+	});
+
+	socket.on('ban_user_on_server', function(data) {
+		var userSocket;
+		for(var i = 0; i < roomUsers.length; i++){
+			for(var j = 0; j < roomUsers[i].length; j++){
+				if(data['bannedUser']==roomUsers[i][j]){
+					bannedUsers[i].push(roomUsers[i][j]);
+					userSocket = roomSockets[i][j];
+				}
+			}
+		}
+		kickUser(data,userSocket);
+	});
+
+	socket.on('kick_user_on_server', function(data) {
+		var userSocket;
+		for(var i = 0; i < roomUsers.length; i++){
+			for(var j = 0; j < roomUsers[i].length; j++){
+				if(data['kickedUser']==roomUsers[i][j]){
+					userSocket = roomSockets[i][j];
+				}
+			}
+		}
+		kickUser(data,userSocket);
 	});
 
 	socket.on('nickname_to_server', function(data) {
@@ -119,40 +153,44 @@ io.sockets.on("connection", function(socket){
 		var otherUsers;
 		for(var i = 0; i < rooms.length; i++){
 			if(rooms[i]==data['room']){
-				if(roomPasswords[i] != "" && roomPasswords[i] != data['password']){
+				socketBanned = false;
+				console.log("BANNED: " + bannedUsers[i]);
+				for(var j = 0; j < bannedUsers[i].length; j++){
+					if(bannedUsers[i][j] === name){
+						socketBanned = true;
+					}
+				}
+				if((roomPasswords[i] != "" && roomPasswords[i] != data['password']) || socketBanned){
 					roomSockets[0].push(socket);
 					roomUsers[0].push(name);
 					otherUsers = roomUsers[0];
+					roomMaker = roomCreators[0];
 					room = "General";
 				}
 				else{
 					roomSockets[i].push(socket);
 					roomUsers[i].push(name);
 					otherUsers = roomUsers[i];
+					roomMaker = roomCreators[i];
 				}
 			}
 		}
-		socket.emit("validate_nickname_to_client", {nickname:name, inRoom:room, users:otherUsers});
+		socket.emit("validate_nickname_to_client", {nickname:name, inRoom:room, users:otherUsers, creator:roomMaker});
 		for(var i = 0; i < roomSockets.length; i++){
-			for(var j = 0; j < roomSockets[i].length; i++){
-				roomSockets[i][j].emit("new_user_joined", {users:otherUsers});
+			for(var j = 0; j < roomSockets[i].length; j++){
+				roomSockets[i][j].emit("new_user_joined", {users:roomUsers[i]});
 			}
 		}
 	});
 });
 
 function removeUser(data, socket){
-	console.log("crntroom: "+data['crntRoom']); // log it to the Node.JS output
-	console.log("crntuser: "+data['crntUser']); // log it to the Node.JS output
 	for(var i = 0; i < rooms.length; i++){
 		if(rooms[i]==data['crntRoom']){
-			console.log("ROOM USERS: "+roomUsers); // log it to the Node.JS output
 			for(var j = 0; j < roomUsers[i].length; j++){
 				if(roomUsers[i][j] == data['crntUser']){
-					console.log("crntroomasjkdhkja: "+roomUsers[i]); // log it to the Node.JS output
 					roomUsers[i].splice(j, 1);
 					roomSockets[i].splice(j, 1);
-					console.log("AHJSJDKHJcrntroomasjkdhkja: "+roomUsers[i]); // log it to the Node.JS output
 				}
 			}
 		}
@@ -164,4 +202,27 @@ function removeUser(data, socket){
 	}
 }
 
-
+function kickUser(data, socket){
+	for(var i = 0; i < rooms.length; i++){
+		if(rooms[i]==data['crntRoom']){
+			for(var j = 0; j < roomUsers[i].length; j++){
+				if(roomUsers[i][j] == data['kickedUser']){
+					roomUsers[i].splice(j, 1);
+					roomSockets[i].splice(j, 1);
+				}
+			}
+		}
+	}
+	for(var i = 0; i < loggedUsers.length; i++){
+		if(loggedUsers[i] == data['kickedUser']){
+			loggedUsers.splice(i,1);
+		}
+	}
+	var name = data['kickedUser'];
+	roomSockets[0].push(socket);
+	roomUsers[0].push(name);
+	var otherUsers = roomUsers[0];
+	var roomMaker = roomCreators[0];
+	room = "General";
+	socket.emit("validate_nickname_to_client", {nickname:name, inRoom:room, users:otherUsers, creator:roomMaker});
+}
